ef43ecaacbff9f1916f868c3817eb59ce7a3c0ab
yang.zheng <yang.zheng@tcl.com>
2018-01-22 18:08:16 +0800

    [cpp] -- update

---
+++ accelerated_cpp/class_11/class_11_0/Vec.cpp
@@ -0,0 +1,109 @@
+#include "Vec.h"
+//#include <iterator>
+using namespace std;
+
+
+//默认构造
+template<class T>
+Vec<T>::Vec(){ create();}
+
+//其他需要明确指定的构造
+template<class T>
+Vec<T>::Vec(typename Vec<T>::size_type n, const T& t){}
+
+//复制构造
+template<class T>
+Vec<T>::Vec(const Vec& v) {}
+
+//析构
+template<class T>
+Vec<T>::~Vec(){
+  uncreate();
+}
+
+//赋值操作
+template<class T>
+Vec<T>& Vec<T>::operator= (const Vec<T>& v){}
+
+//索引操作
+template<class T>
+T& Vec<T>::operator[] (typename Vec<T>::size_type i){
+  return data[i];
+}
+
+template<class T>
+const T& Vec<T>::operator[] (typename Vec<T>::size_type i) const {
+  return data[i];
+}
+
+//通用接口操作
+
+template<class T>
+typename Vec<T>::iterator Vec<T>::begin(){
+  return data;
+}
+
+template<class T>
+typename Vec<T>::const_iterator Vec<T>::begin() const {
+  return data;
+}
+
+template<class T>
+typename Vec<T>::iterator Vec<T>::end() {
+  return avail;
+}
+
+template<class T>
+typename Vec<T>::const_iterator Vec<T>::end() const {
+  return avail;
+}
+
+template<class T>
+typename Vec<T>::size_type Vec<T>::size() const {
+  return avail-data;
+}
+
+template<class T>
+void Vec<T>::push_back(T& t){
+
+}
+
+
+//内存操作函数
+template<class T>
+void Vec<T>::uncreate(){
+  if (data){
+    iterator it = avail;
+    while (it != data)
+      alloc.destory(--it); //看到这里的--it就是为了实现逆序删除元素.
+    alloc.deallocate(data,limit-data);
+  }
+  data = limit = avail = 0;
+}
+
+template<class T>
+void Vec<T>::graw(){
+  iterator new_data = alloc.allocate(max(ptrdiff_t(1), 2*(limit-data)));
+}
+
+template<class T>
+void Vec<T>::unchecked_append(){}
+
+//重载 create函数
+template<class T>
+void Vec<T>::create(){
+  return data = avail = limit = 0;
+}
+
+template<class T>
+void Vec<T>::create(typename Vec<T>::size_type n, const T& val){ //这里的typename可以不用
+  data = alloc.allocate(n);
+  avail = limit = data + n;
+  uninitialized_fill(data, avail, val);
+}
+
+template<class T>
+void Vec<T>::create(Vec::const_iterator i, Vec::const_iterator j){
+  data = alloc.allocate(j - i);
+  uninitialized_copy(i, j, data);
+}
---
+++ accelerated_cpp/class_11/class_11_0/Vec.h
@@ -0,0 +1,50 @@
+#include <memory>
+#include <cstddef>
+
+template <class T>
+class Vec {
+public:
+  //统一别名.
+  typedef T* iterator;
+  typedef const T* const_iterator;
+  typedef size_t size_type;
+  typedef T value_type;
+
+  //默认构造,加上三位一体
+  Vec();
+  explicit Vec(size_type n, const T& = T());
+  Vec(const Vec&);
+  ~Vec();
+  Vec& operator= (const Vec&); // 这个返回值可以没有的,但是为了统一可以加上.
+
+  //索引操作.
+  T& operator[] (size_type i);
+  const T& operator[] (size_type i) const;
+
+  //通用接口
+  iterator begin();
+  const_iterator begin () const;
+  iterator end();
+  const_iterator end () const;
+  size_type size  () const;
+  void push_back(T&);
+
+
+private:
+  //容器中的迭代器
+  iterator data;
+  iterator avail;
+  iterator limit;
+
+  std::allocator<T> alloc;
+
+  void create();
+  void create(size_type , const T&);
+  void create(const_iterator, const_iterator);
+
+  void uncreate();
+
+  void graw();
+  void unchecked_append();
+
+};
